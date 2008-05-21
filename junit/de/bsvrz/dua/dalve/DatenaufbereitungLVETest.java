0ad62efd75bc01d02ec99d99cbb227e7370887fb
goerlitz <info@bitctrl.de>
2008-05-21 09:21:12 +0000

    Neue Testdaten vom 20.05.08
    git-svn-id: svn://ntoserver/Datenverteiler/VRZ3/trunk/de.bsvrz.dua.dalve@9051 40c0be50-5d29-0410-901d-fb59edb5cafc

--- junit/de/bsvrz/dua/dalve/DatenaufbereitungLVETest.java
+++ junit/de/bsvrz/dua/dalve/DatenaufbereitungLVETest.java
@@ -45,7 +45,8 @@ public class DatenaufbereitungLVETest {
 //	protected static final String TEST_DATEN_VERZ = ".\\extra\\testDaten\\V_2.8.7 (09.05.08)\\"; //$NON-NLS-1$
 //	protected static final String TEST_DATEN_VERZ = ".\\extra\\testDaten\\V_2.8.8 (15.05.08)\\"; //$NON-NLS-1$
 //	protected static final String TEST_DATEN_VERZ = ".\\extra\\testDaten\\V_2.8.9 (15.05.08)\\"; //$NON-NLS-1$
-	protected static final String TEST_DATEN_VERZ = ".\\extra\\testDaten\\V_2.9.1 (19.05.08)\\"; //$NON-NLS-1$
+//	protected static final String TEST_DATEN_VERZ = ".\\extra\\testDaten\\V_2.9.1 (19.05.08)\\"; //$NON-NLS-1$
+	protected static final String TEST_DATEN_VERZ = ".\\extra\\testDaten\\V_2.9.2 (20.05.08)\\"; //$NON-NLS-1$
 
 	
 	/**
